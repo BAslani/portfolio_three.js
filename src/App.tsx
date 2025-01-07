dbe7fdc2ad5ba078d06d488a59edd70dff80a9bf
behzad aslani <behzadaslani80@gmail.com>
2025-01-07 13:28:24 +0330

    feat: implemented Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import Navbar from './sections/Navbar'
+
 export default function App() {
-  return <div className='text-white'>App</div>
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar />
+    </main>
+  )
 }
